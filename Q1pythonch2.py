e7280217cba41818a2ecff1e634e5a4ccea83d2b
MahaKhudhair <41030892+MahaKhudhair@users.noreply.github.com>
2018-08-05 04:20:07 +0300

    Add files via upload

---
+++ Q1pythonch2.py
@@ -0,0 +1,16 @@
+Python 3.7.0b5 (v3.7.0b5:abb8802389, May 31 2018, 01:54:01) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> L = [ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ]
+>>> CL = [ print (num**2) for num in L ]
+0
+1
+4
+9
+16
+25
+36
+49
+64
+81
+100
+>>> 
